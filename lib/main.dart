bcf5e965e8e3449536006431fffe5f09f29bc841
Phils Garcia <eivanphils@gmail.com>
2022-06-07 23:10:01 -0400

    Agregada peticion de las peliculas recientes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'package:provider/provider.dart';
+
+import 'package:flutter_xtream_movies/providers/movies_provider.dart';
 import 'package:flutter_xtream_movies/router/app_routes.dart';
 import 'package:flutter_xtream_movies/theme/app_theme.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MoviesProvider(), lazy: false,)
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'package:flutter_xtream_movies/models/movies_request.dart';
+
+class MoviesProvider extends ChangeNotifier {
+  MoviesProvider() {
+    print('MoviesProvider inicializado');
+
+    getMoviesNowPlaying();
+  }
+
+  getMoviesNowPlaying() async {
+    print('getNowPlaying');
+
+    final url = Uri.parse(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=da667a4fafb64048d77c2deeecb0ed26&language=en-ES&external_source=imdb_id');
+
+    http.get(url).then((response) {
+      final reqResRespuesta = moviesRequestFromJson(response.body);
+
+      print(reqResRespuesta);
+      print('dates: ${reqResRespuesta.dates}');
+      print('totalPages: ${reqResRespuesta.totalPages}');
+      print('totalResults: ${reqResRespuesta.totalResults}');
+      print('result[0]: ${reqResRespuesta.results[0].adult}');
+    });
+  }
+}
