6e30cb6906ea81061fdda047b3c8c24b610e420b
Phils Garcia <eivanphils@gmail.com>
2022-06-08 15:26:02 -0400

    Update movies_provider.dart

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -26,5 +26,27 @@ class MoviesProvider extends ChangeNotifier {
       print('totalResults: ${reqResRespuesta.totalResults}');
       print('result[0]: ${reqResRespuesta.results[0].adult}');
     });
+    
+  
+    /* ejemplo con el await
+    final url = Uri.https(
+      'api.themoviedb.org',
+      '3/movie/now_playing',
+      {
+        'api_key': 'da667a4fafb64048d77c2deeecb0ed26',
+        'language': 'en-ES',
+        'page': '1'
+      }
+    );
+    final response = await http.get(url);
+    final reqResRespuesta = moviesRequestFromJson(response.body);
+
+
+    print(reqResRespuesta);
+    print('dates: ${reqResRespuesta.dates}');
+    print('totalPages: ${reqResRespuesta.totalPages}');
+    print('totalResults: ${reqResRespuesta.totalResults}');
+    print('result[0]: ${reqResRespuesta.results[0].adult}');
+    */
   }
 }
