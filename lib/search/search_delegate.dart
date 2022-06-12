b645b04fa820b5402f26b966daa73c47d265bad8
Phils Garcia <eivanphils@gmail.com>
2022-06-11 21:28:22 -0400

    Agregada estructura del buscador

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -7,29 +7,36 @@ class MovieSearchDelegate extends SearchDelegate {
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      IconButton(
-        onPressed: () => Navigator.pushNamed(context, 'movies'),
-        icon: const Icon(Icons.search)
-      )
+      IconButton(onPressed: () => query = '', icon: const Icon(Icons.clear))
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
-      onPressed: () => Navigator.pushNamed(context, 'movies'),
-      icon: const Icon(Icons.arrow_back)
-      );
+        onPressed: () {
+          close(context, null);
+        },
+        icon: const Icon(Icons.arrow_back));
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    return Text('results');
-
+    return const Text('results');
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Text('suggestions: $query');
+    if (query.isEmpty) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Center(child: Image(image: AssetImage('assets/empty.png'))),
+          SizedBox(height: 10),
+          Text('No se han realizado busquedas', style: TextStyle(fontSize: 16),),
+        ],
+      );
+    }
+    return Container();
   }
 }
